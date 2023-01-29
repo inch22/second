b815586e327b4845b0ca48a1b709fe9baf8034cb
inch22 <inch22@mail.ru>
2023-01-29 20:37:43 +0300

    second

--- tests/test_check_text_please.py
+++ tests/test_check_text_please.py
@@ -14,7 +14,6 @@ def test_page_elements(browser):
     elements_page.visit()
     assert elements_page.text_please.get_text() == 'Please select an item from left to start practice.'
     assert elements_page.text_element.get_text() == 'Elements'
-
     assert elements_page.icon.exist()
     assert elements_page.btn_sidebar_first.exist()
     assert elements_page.btn_sidebar_first.textbox.exist()
