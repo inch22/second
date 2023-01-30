db70ea9812fa0327c99ac0c6f48d6f43ff86630c
inch22 <inch22@mail.ru>
2023-01-30 14:10:13 +0300

    second

--- page/base_page.py
+++ page/base_page.py
@@ -3,16 +3,28 @@ from selenium.webdriver.common.by import By
 class BasePage:
     def __init__(self, driver, base_url):
         self.driver = driver
-        self.base_url = base_url #"https://demoqa.com/"
+        self.base_url = base_url #"https://demoqa.com/" базовая страница
 
     def visit(self):
-        return self.driver.get(self.base_url)
+        return self.driver.get(self.base_url) #
 
+    def back(self):
+        self.driver.back() #стрелка назад в браузере
+
+    def forward(self):
+        self.driver.forward() #стрелка вперед в браузере
+
+    def refresh(self):
+        self.driver.refresh() #обновить страницу
 
     def get_url(self):
-        return self.driver.current_url
+        return self.driver.current_url #получить текущий url
 
-    def equal_url(self): #создание метода для проверки урла, чтобв проверить, что находимся на нужной странице
+    def get_title(self):
+        return self.driver.title
+
+    def equal_url(self): #метод должен вызывать get_url сравнивать его со строкой 'https://demoqa.com/'
+                         #возвращать True если проверка пройдена и False если нет
         if self.get_url() == self.base_url:
             return True
         return False
