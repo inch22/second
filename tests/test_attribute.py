684c41e4da5014c51604ceb32b996077408fc9eb
inch22 <inch22@mail.ru>
2023-02-08 21:01:00 +0300

    second

--- tests/test_attribute.py
+++ tests/test_attribute.py
@@ -2,12 +2,18 @@ import time
 from page.textbox_page import TextboxPage
 
 
+@allure.feature('check attr')
+@allure.story('проверка значения атрибута элемента')
+@allure.severity(allure.severity_level.BLOCKER)
 def test_placeholder(browser):
     """ проверка значения атрибута элемента """
     atrb1 = TextboxPage(browser)
     atrb1.visit()
     assert atrb1.full_name.get_dom_attribute('placeholder') == 'Full Name'
 
+@allure.feature('check attr')
+@allure.story('проверка наличия атрибута элемента')
+@allure.severity(allure.severity_level.BLOCKER)
 def test_attr_exist(browser):
     """ проверка наличия атрибута элемента """
     atrb2 = TextboxPage(browser)
@@ -15,6 +21,9 @@ def test_attr_exist(browser):
     atrb2.btn_first.click_elem()
     assert atrb2.box_first_menu.get_dom_attribute('style')
 
+@allure.feature('check attr')
+@allure.story('проверка отсутствия атрибута элемента')
+@allure.severity(allure.severity_level.BLOCKER)
 def test_attr_not_exist(browser):
     """ проверка отсутствия атрибута элемента """
     atrb3 = TextboxPage(browser)
